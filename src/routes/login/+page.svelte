2065c370af02bbf3d9c2d43b79bc437f102271f9
abbottjord94 <abbottjord94@gmail.com>
2025-03-24 23:28:51 -0700

    some spacing changes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,9 @@
 <div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 p-4 mx-64 my-32 text-center">
     <img src="/rootaccess-logo-color.svg" alt="Root Access S.H.I.E.L.D" class="h-12 w-auto mb-16" />
     <p class="text-2xl font-bold">Login</p>
-    <div class="items-center">
-        <input type="text" class="input p-4 mx-16 my-4" placeholder="Username" required/>
-        <input type="password" class="input p-4 mx-16 my-4" placeholder="Password" required/>
+    <div class="items-center justify-between p-4">
+        <input type="text" class="input p-4 m-4" placeholder="Username" required/>
+        <input type="password" class="input p-4 m-4" placeholder="Password" required/>
         <button type="button" class="btn preset-filled-primary-500 m-4" onclick={
             ()=> {
                 //TODO: Actual authentication logic
