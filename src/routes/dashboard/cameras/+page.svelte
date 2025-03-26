f593cc6e6e49aae7dd8491ad1af51e8d880d3a13
abbottjord94 <abbottjord94@gmail.com>
2025-03-25 21:14:04 -0700

    added a back link

--- src/routes/dashboard/cameras/+page.svelte
+++ src/routes/dashboard/cameras/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    import { MoveLeft } from "@lucide/svelte";
     import { Paths } from "$lib/consts/paths";
     import Container from "$lib/components/Containers/Container.svelte";
+	import BackLink from "$lib/components/Links/BackLink/BackLink.svelte";
 </script>
 
 <Container>
-    <a class="flex items-center" href={Paths.dashboard.index}><MoveLeft /><span class="text-xs ml-4">Back To Dashboard</span></a>
+    <BackLink path={Paths.dashboard.index} text="Back To Dashboard" />
 </Container>
